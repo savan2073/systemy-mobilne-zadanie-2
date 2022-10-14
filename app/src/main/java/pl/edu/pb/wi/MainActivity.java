11fbc4fdb59b4c761625a5917da22805b4305fd1
savan2073 <savan2073@gmail.com>
2022-10-14 12:52:20 +0200

    27 not tested

--- app/src/main/java/pl/edu/pb/wi/MainActivity.java
+++ app/src/main/java/pl/edu/pb/wi/MainActivity.java
@@ -1,6 +1,7 @@
 package pl.edu.pb.wi;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -15,6 +16,9 @@ public class MainActivity extends AppCompatActivity {
     private static final String KEY_CURRENT_INDEX = "currentIndex";
     private static final String QUIZ_TAG = "MainActivity";
     public static final String KEY_EXTRA_ANSWER = "pl.edu.pb.wi.quiz.correctAnswer";
+    private static final int REQUEST_CODE_PROMPT = 0;
+
+    private boolean answerWasShown;
 
     private Button trueButton;
     private Button falseButton;
@@ -65,6 +69,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v){
                 currentIndex=(currentIndex+1)%questions.length;
+                answerWasShown = false;
                 setNextQuestion();
             }
         });
@@ -74,7 +79,7 @@ public class MainActivity extends AppCompatActivity {
                 Intent intent = new Intent(MainActivity.this,PromptActivity.class);
                 boolean correctAnswer = questions[currentIndex].isTrueAnswer();
                 intent.putExtra(KEY_EXTRA_ANSWER,correctAnswer);
-                startActivity(intent);
+                startActivityForResult(intent, REQUEST_CODE_PROMPT);
             }
         });
         setNextQuestion();
@@ -117,14 +122,33 @@ public class MainActivity extends AppCompatActivity {
         outState.putInt(KEY_CURRENT_INDEX,currentIndex);
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(resultCode != RESULT_OK){
+            return;
+        }
+        if(requestCode == REQUEST_CODE_PROMPT){
+            if(data == null){
+                return;
+            }
+            answerWasShown = data.getBooleanExtra(PromptActivity.KEY_EXTRA_ANSWER_SHOWN, false);
+        }
+    }
+
     private void checkAnswerCorrectness(boolean userAnswer) {
         boolean correctAnswer = questions[currentIndex].isTrueAnswer();
         int resultMessageId = 0;
-        if(userAnswer == correctAnswer){
-            resultMessageId = R.string.correct_answer;
+        if(answerWasShown){
+            resultMessageId = R.string.answer_was_shown;
         }else{
-            resultMessageId = R.string.incorrect_answer;
+            if(userAnswer == correctAnswer){
+                resultMessageId = R.string.correct_answer;
+            }else{
+                resultMessageId = R.string.incorrect_answer;
+            }
         }
+
         Toast.makeText(this,resultMessageId, Toast.LENGTH_SHORT).show();
     }
 
--- app/src/main/java/pl/edu/pb/wi/PromptActivity.java
+++ app/src/main/java/pl/edu/pb/wi/PromptActivity.java
@@ -2,6 +2,7 @@ package pl.edu.pb.wi;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -9,9 +10,12 @@ import android.widget.TextView;
 
 public class PromptActivity extends AppCompatActivity {
 
+    public static final String KEY_EXTRA_ANSWER_SHOWN = "pb.edu.pl.wi.quiz.answerShown";
+
     private boolean correctAnswer;
 
     private Button showCorrectAnswerButton;
+    private Button returnMainButton;
     private TextView answerTextViev;
 
     @Override
@@ -20,6 +24,7 @@ public class PromptActivity extends AppCompatActivity {
         setContentView(R.layout.activity_prompt);
 
         showCorrectAnswerButton = findViewById(R.id.show_answer_button);
+        returnMainButton = findViewById(R.id.return_main_button);
         answerTextViev = findViewById(R.id.answer_text_view);
 
         correctAnswer = getIntent().getBooleanExtra(MainActivity.KEY_EXTRA_ANSWER,true);
@@ -29,7 +34,22 @@ public class PromptActivity extends AppCompatActivity {
             public void onClick(View view) {
                 int answer = correctAnswer ? R.string.button_true : R.string.button_false;
                 answerTextViev.setText(answer);
+                setAnswerShownResult(true);
             }
         });
+
+        returnMainButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(PromptActivity.this,MainActivity.class);
+            }
+        });
+
+
+    }
+    private void setAnswerShownResult(boolean answerWasShown){
+        Intent resultIntent = new Intent();
+        resultIntent.putExtra(KEY_EXTRA_ANSWER_SHOWN, answerWasShown);
+        setResult(RESULT_OK, resultIntent);
     }
 }
